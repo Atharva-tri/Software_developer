f9c02a8e151f905d97e33d5f5ef96093434ce5be
Sathivika Pittala <sathivikapittala2005@gmail.com>
2026-01-07 12:30:31 +0530

    Pyramid Pattern code

---
+++ pattern.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class pattern {
+    public static void main(String[] args){
+        int n=5;
+
+        for(int i=1;i<=n;i++){
+           
+            for(int space = 1; space <= n-i; space++){
+
+                System.out.print(" ");
+
+            }
+
+            for(int start = 1; start<=(2*i-1); start++){
+                System.out.print("*");
+                
+            }
+
+            
+
+            System.out.println();
+
+
+
+
+        }
+
+
+    }
+
+}
